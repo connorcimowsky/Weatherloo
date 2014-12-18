1d703907eb508d8b8c2ca18304e31c8620ec2f1b
Connor Cimowsky <connorcimowsky@me.com>
2014-12-18 18:54:38 -0500

    Get rid of the janky status item animation.

--- Weatherloo/StatusBarButtonItem.swift
+++ Weatherloo/StatusBarButtonItem.swift
@@ -8,62 +8,25 @@
 
 import UIKit
 
-class ContainerView: UIView {
-    var label: UILabel {
-        didSet {
-            self.label.alpha = 0.0
-            self.addSubview(self.label)
-            
-            UIView.animateWithDuration(0.3, delay: 0.0, options: .BeginFromCurrentState, animations: {
-                oldValue.alpha = 0.0
-                self.label.alpha = 1.0
-            }, completion: { finished in
-                oldValue.removeFromSuperview()
-            })
-        }
-    }
-    
-    required override init() {
-        self.label = UILabel()
-        super.init()
-    }
-
-    required init(coder aDecoder: NSCoder) {
-        self.label = UILabel()
-        super.init(coder: aDecoder)
-    }
-    
-    required override init(frame: CGRect) {
-        self.label = UILabel()
-        super.init(frame: frame)
-    }
-    
-    override func sizeThatFits(size: CGSize) -> CGSize {
-        return label.sizeThatFits(size)
-    }
-}
-
 class StatusBarButtonItem: UIBarButtonItem {
-    
-    var containerView = ContainerView()
+    var label: UILabel
     var text: String? {
         get {
-            return self.containerView.label.text
+            return self.label.text
         }
         
         set {
-            let newLabel = UILabel()
-            newLabel.text = newValue
-            newLabel.sizeToFit()
-            
-            self.containerView.label = newLabel
-            self.containerView.sizeToFit()
+            self.label.text = newValue
+            self.label.sizeToFit()
         }
     }
     
     required init(coder aDecoder: NSCoder) {
+        self.label = UILabel()
+        self.label.font = UIFont.systemFontOfSize(UIFont.systemFontSize())
+        
         super.init(coder: aDecoder)
         
-        self.customView = self.containerView
+        self.customView = self.label
     }
 }
