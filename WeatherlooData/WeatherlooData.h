55de053ff3231f63354f44e0b25e5564ce264ad8
Connor Cimowsky <connorcimowsky@me.com>
2014-12-21 11:22:03 -0500

    Add a target for an embedded framework.

---
+++ WeatherlooData/WeatherlooData.h
@@ -0,0 +1,13 @@
+//
+//  WeatherlooData.h
+//  WeatherlooData
+//
+//  Created by Connor Cimowsky on 12/21/14.
+//  Copyright (c) 2014 Connor Cimowsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+FOUNDATION_EXPORT double WeatherlooDataVersionNumber;
+
+FOUNDATION_EXPORT const unsigned char WeatherlooDataVersionString[];
---
+++ WeatherlooDataTests/WeatherlooDataTests.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherlooDataTests.swift
+//  WeatherlooDataTests
+//
+//  Created by Connor Cimowsky on 12/21/14.
+//  Copyright (c) 2014 Connor Cimowsky. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class WeatherlooDataTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+}
